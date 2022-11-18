e3deb72bd9abbfbe26f2d87bf0187fb8d978d75a
Nephtali Ceballos <elegy0101@gmail.com>
2022-11-17 21:32:34 -0600

    Adds migrations

--- tests/functional/tests/controllers/usersControllers.test.js
+++ tests/functional/tests/controllers/usersControllers.test.js
@@ -1,26 +1,31 @@
+
 const request = require('supertest')
 const app = require('../../../../app')
+
 const { seedUser, getUser } = require('../../utils/users')
 const { clearDatabase  } = require('../../utils/db')
 
+
 describe('usersController', () => {
-    beforeEach( async () => {
+    beforeEach(async () => {
         await clearDatabase()
     })
     describe('updateUser', () => {
-        test('It should a return a 404 if the user is not found!', async () => {
-                const result = await request(app).put('/api/users/5').send({ name: 'Robin' })
-                    .expect(404)
+        test('it should return a 404 if the user is not found', async () => {
+            await request(app).put('/api/users/11').send({ name: 'Robin' })
+                .expect(404)
         })
-       test('It should update a found user', async () => {
-               const user = await seedUser('Batman')
-               const name = 'Robin'
-               await request(app).put(`/api/users/${user.id}`).send({ name: 'Robin' })
-                   .expect(200)
+        test('it should update a found user', async () => {
+            const user = await seedUser('Batman')
 
-               const updatedUser = await getUser(user.id)
-               expect(updatedUser.name).toEqual(name)
+            const name = 'Robin'
+            await request(app).put(`/api/users/${user.id}`).send({ name: 'Robin' })
+                .expect(200 )
 
-       })
+            const updatedUser = await getUser(user.id)
+            expect(updatedUser.name).toEqual(name)
+
+        })
+
     })
 })
